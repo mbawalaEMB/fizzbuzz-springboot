b199f981a75d10267018033d4f3a9fa723a46c00
Emmanuel Mbawala <erumuli03@gmail.com>
2022-05-02 07:09:46 +0200

    cleaned a little bit

--- src/main/java/com/emb/main/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/emb/main/fizzbuzz/FizzBuzz.java
@@ -5,25 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-
-/**
- * Schreibe eine kleine Spring-Boot-Anwendung mit einer REST-API, die einen 
- * Eingabewert empfängt und eine Ergebnisliste im JSON-Format zurückgibt.
-Durch die Eingabe einer Zahl gibt die Anwendung eine Liste von Werten beginnend mit 1 
-bis zur eingegebenen Zahl unter Berücksichtigung der folgenden Regeln zurück:
-
-- Wenn die Zahl durch 3 teilbar ist, dann soll ‚Fizz‘ ausgegeben werden
-- Wenn Zahl durch 5 teilbar ist, soll ‚Buzz‘ ausgeben werden
-- Wenn die Zahl sowohl durch 3 als auch durch 5 teilbar ist, dann soll ‚FizzBuzz‘ ausgeben werden
-- Andernfalls soll nur die nächste Zahl ausgeben werden
-
- 
-
-Hinweise:  Verwende TDD, erkläre die Schritte, die Du durchgeführt, und beachte Fehlerfälle.
-                    Es ist nicht notwendig, eine Benutzeroberfläche zu entwickeln.
-
- 
- * */
 public class FizzBuzz {
 	
 	private List<String> fizzbuzz;
